70c21f383026a76ba75446d5b71c7c3bd698b024
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-05-20 19:49:28 +0800

    snippets

--- src/App.js
+++ src/App.js
@@ -8,64 +8,64 @@ function App() {
     e.preventDefault();
     let index = e.target.value;
     //only delete 1 element
-    todos.splice(index,1);
+    todos.splice(index, 1);
     setTodos([...todos]);
   }
-//we need a list of todo's
-//useState[params, funcyion]        init params
-  const [todos, setTodos] = useState([]); 
+  //we need a list of todo's
+  //useState[params, funcyion]        init params
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
 
     //prevent refresh
     e.preventDefault();
     //[],'walk the dog'
     //['walk the dog']
     //put input at the end of todos =>[...todos,]
-    setTodos([input,...todos]);
+    setTodos([input, ...todos]);
     //clear input
     setInput("");
   }
 
-   /* form allow to submitt sth */
+  /* form allow to submitt sth */
   return (
     <div className="App">
       <div className="title">
-        <h1>To-do APP</h1>  
+        <h1>To-do APP</h1>
       </div>
-      
-     <div className="todo-container">
+
+      <div className="todo-container">
         <div className="todo-bar">
           <form>
-          <input value={input}
-            onChange={e =>setInput(e.target.value)}
+            <input value={input}
+              onChange={e => setInput(e.target.value)}
               type="text"
-          />
-        
-          <button disabled={!input} type="submit"
-            onClick={handleSubmit}>
+            />
+
+            <button disabled={!input} type="submit"
+              onClick={handleSubmit}>
               Add todo
           </button>
           </form>
-          </div>
-      <div className="todo-content">
-        {todos.map((todo, index)=>(
-          <Todo
+        </div>
+        <div className="todo-content">
+          {todos.map((todo, index) => (
+            <Todo
 
-            key = {index}
-            value={index}
-            title={todo}
-            deleteTodo={deleteTodo}
-           />
-        ))}
+              key={index}
+              value={index}
+              title={todo}
+              deleteTodo={deleteTodo}
+            />
+          ))}
 
-      
-       </div>
-      
-     </div>
-      
-      
-     
+
+        </div>
+
+      </div>
+
+
+
     </div>
   );
 }
